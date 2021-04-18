fcee544391b3a81fa570e291562ea102cda3347e
Sparrow1001 <captfl1nt@yandex.ru>
2021-04-19 00:09:06 +0300

    Add user profile

--- app/src/main/java/com/example/tifigosocialmedia/LoginActivity.java
+++ app/src/main/java/com/example/tifigosocialmedia/LoginActivity.java
@@ -251,20 +251,24 @@ public class LoginActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
 
-                            String email = user.getEmail();
-                            String uid = user.getUid();
+                            if (task.getResult().getAdditionalUserInfo().isNewUser()){
+                                assert user != null;
+                                String email = user.getEmail();
+                                String uid = user.getUid();
 
-                            HashMap<Object, String> hashMap = new HashMap<>();
-                            hashMap.put("email", email);
-                            hashMap.put("uid", uid);
-                            hashMap.put("name", ""); //TODO
-                            hashMap.put("phone", "");
-                            hashMap.put("image", "");
+                                HashMap<Object, String> hashMap = new HashMap<>();
+                                hashMap.put("email", email);
+                                hashMap.put("uid", uid);
+                                hashMap.put("name", ""); //TODO
+                                hashMap.put("phone", "");
+                                hashMap.put("image", "");
 
-                            FirebaseDatabase database =FirebaseDatabase.getInstance();
-                            DatabaseReference reference = database.getReference("Users");
+                                FirebaseDatabase database =FirebaseDatabase.getInstance();
+                                DatabaseReference reference = database.getReference("Users");
 
-                            reference.child(uid).setValue(hashMap);
+                                reference.child(uid).setValue(hashMap);
+                            }
+
 
                             Toast.makeText(LoginActivity.this, ""+user.getEmail(), Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
