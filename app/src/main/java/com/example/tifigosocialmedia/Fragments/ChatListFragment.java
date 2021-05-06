70209568bde8f6ad924b8391e12ebb31607c18ad
Sparrow1001 <592001@mail.ru>
2021-05-06 13:27:07 +0300

    Add chat list

--- app/src/main/java/com/example/tifigosocialmedia/Fragments/ChatListFragment.java
+++ app/src/main/java/com/example/tifigosocialmedia/Fragments/ChatListFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -17,25 +18,134 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
 
+import com.example.tifigosocialmedia.Adapters.AdapterChatlist;
 import com.example.tifigosocialmedia.MainActivity;
+import com.example.tifigosocialmedia.Models.ModelChat;
+import com.example.tifigosocialmedia.Models.ModelChatlist;
+import com.example.tifigosocialmedia.Models.ModelUsers;
 import com.example.tifigosocialmedia.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ChatListFragment extends Fragment {
 
     FirebaseAuth firebaseAuth;
+    RecyclerView recyclerView;
+    List<ModelChatlist> chatlistList;
+    List<ModelUsers> usersList;
+    DatabaseReference reference;
+    FirebaseUser currentUser;
+    AdapterChatlist adapterChatlist;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        firebaseAuth = FirebaseAuth.getInstance();
-
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+        chatlistList = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chatlist").child(currentUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                chatlistList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    ModelChatlist chatlist = ds.getValue(ModelChatlist.class);
+                    chatlistList.add(chatlist);
+                }
+                loadChats();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         return view;
+    }
+
+    private void loadChats() {
+        usersList = new ArrayList<>();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                usersList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    ModelUsers user = ds.getValue(ModelUsers.class);
+                    for(ModelChatlist chatlist: chatlistList){
+                        if (user.getUid() != null && user.getUid().equals(chatlist.getId())){
+                            usersList.add(user);
+                            break;
+                        }
+                    }
+
+                    adapterChatlist = new AdapterChatlist(getContext(), usersList);
+
+                    recyclerView.setAdapter(adapterChatlist);
+
+                    for(int i=0; i<usersList.size(); i++){
+                        lastMessage(usersList.get(i).getUid());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void lastMessage(String userId) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String theLastMessage = "default";
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    ModelChat chat = ds.getValue(ModelChat.class);
+                    if (chat == null){
+                        continue;
+                    }
+                    String sender = chat.getSender();
+                    String receiver = chat.getReceiver();
+                    if (sender == null || receiver == null){
+                        continue;
+                    }
+                    if (chat.getReceiver().equals(currentUser.getUid()) && chat.getSender().equals(userId) || chat.getReceiver().equals(userId) && chat.getSender().equals(currentUser.getUid())){
+
+                        theLastMessage = chat.getMessage();
+
+                    }
+                }
+                adapterChatlist.setLastMessageMap(userId, theLastMessage);
+                adapterChatlist.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void checkUserStatus(){
--- app/src/main/java/com/example/tifigosocialmedia/Models/ModelUsers.java
+++ app/src/main/java/com/example/tifigosocialmedia/Models/ModelUsers.java
@@ -2,13 +2,13 @@ package com.example.tifigosocialmedia.Models;
 
 public class ModelUsers {
 
-    String name, email, search, phone, image, cover, uid;
+    String name, email, search, phone, image, cover, uid, onlineStatus, typingTo;
 
 
     public ModelUsers() {
     }
 
-    public ModelUsers(String name, String email, String search, String phone, String image, String cover, String uid) {
+    public ModelUsers(String name, String email, String search, String phone, String image, String cover, String uid, String onlineStatus, String typingTo) {
         this.name = name;
         this.email = email;
         this.search = search;
@@ -16,14 +16,8 @@ public class ModelUsers {
         this.image = image;
         this.cover = cover;
         this.uid = uid;
-    }
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
+        this.onlineStatus = onlineStatus;
+        this.typingTo = typingTo;
     }
 
     public String getName() {
@@ -72,5 +66,29 @@ public class ModelUsers {
 
     public void setCover(String cover) {
         this.cover = cover;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getOnlineStatus() {
+        return onlineStatus;
+    }
+
+    public void setOnlineStatus(String onlineStatus) {
+        this.onlineStatus = onlineStatus;
+    }
+
+    public String getTypingTo() {
+        return typingTo;
+    }
+
+    public void setTypingTo(String typingTo) {
+        this.typingTo = typingTo;
     }
 }
